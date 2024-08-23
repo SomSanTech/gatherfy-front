cdfed0e74ef01c69c14dd35c57c80172bf83c191
sealbb <chonticha.licha@gmail.com>
2024-08-24 01:55:32 +0700

    test: test format tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,6 @@ const hello2 = 'aaa';
 </script>
 
 <template>
-  <h1>Hello this is Gatherfy kub pom!</h1>
+  <h1 class="text-2xl mx-auto font-bold">Hello this is Gatherfy kub pom!</h1>
+  <h1 class="text-2xl  font-bold mx-auto">Hello this is Gatherfy kub pom!</h1>
 </template>
