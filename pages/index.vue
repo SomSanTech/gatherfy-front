b3fd51408f57de036b523fa3ef2bd6d4d2ff584d
sealbb <chonticha.licha@gmail.com>
2024-08-24 01:56:11 +0700

    test: lint-staged

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,6 @@ const hello2 = 'aaa';
 </script>
 
 <template>
-  <h1 class="text-2xl mx-auto font-bold">Hello this is Gatherfy kub pom!</h1>
-  <h1 class="text-2xl  font-bold mx-auto">Hello this is Gatherfy kub pom!</h1>
+  <h1 class="mx-auto text-2xl font-bold">Hello this is Gatherfy kub pom!</h1>
+  <h1 class="mx-auto text-2xl font-bold">Hello this is Gatherfy kub pom!</h1>
 </template>
