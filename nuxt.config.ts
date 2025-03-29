c3b0736a6197294fdab3b95fe6f6d991d9003a5a
sealbb <chonticha.licha@gmail.com>
2025-03-29 18:53:47 +0700

    fix: update json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['nuxt-rating', 'nuxt-vue3-google-signin'],
+
   googleSignIn: {
     clientId:
       '208535017949-i5clt2a567g51nhu9lj58ctdqo8vkp2i.apps.googleusercontent.com',
   },
+
   // compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   css: ['~/assets/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   components: {
     dirs: [
       { path: '~/components', pathPrefix: false, extensions: ['vue'] },
@@ -26,17 +31,22 @@ export default defineNuxtConfig({
       { path: '~/components/icons', pathPrefix: false },
     ],
   },
+
   typescript: {
     strict: false,
   },
+
   runtimeConfig: {
     public: {
       baseUrl: process.env.NUXT_PUBLIC_BASE_URL,
       minioUrl: process.env.MINIO_URL,
     },
   },
+
   app: {
     baseURL: process.env.NUXT_BASE_URL,
   },
+
   plugins: ['~/plugins/preline.client.ts', '~/plugins/error-handler.ts'],
+  compatibilityDate: '2025-03-29',
 });
